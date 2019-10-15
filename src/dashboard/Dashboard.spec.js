39efc277f92fce335c174671b3feaa8cc30d323e
Vyraal1 <kevin.afable@gmail.com>
2019-10-15 17:31:06 -0400

    dashboard testing bam

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from "react";
+import Dashboard from "./Dashboard";
+import Controls from "../controls/Controls";
+import { render } from "@testing-library/react";
+
+test("App render", () => {
+  render(<Dashboard />);
+});
+
+test("Gate defaults to 'unlocked' and 'open'", () => {
+  // returns a method that returns a component with the given text
+  const { getByText } = render(<Dashboard />);
+  expect(getByText(/open/i));
+  expect(getByText(/unlocked/i));
+});
+
+//am i missing anything else?
