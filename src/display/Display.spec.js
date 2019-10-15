d2216862318d49086841541e14f63043ebb9617d
Vyraal1 <kevin.afable@gmail.com>
2019-10-15 17:31:19 -0400

    display testing, learning queryByText

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,25 @@
-// Test away!
+import React from "react";
+import { render, queryByText } from "@testing-library/react";
+import Display from "./Display";
+
+test("Display renders", () => {
+  render(<Display />);
+});
+
+test(`Display 'Locked' if the locked prop is true, and 'Unlocked' if false`, () => {
+  const { getByText, queryByText } = render(<Display locked={false} />);
+  expect(getByText(/Unlocked/i));
+  expect(queryByText(/^Locked/i)).toBeFalsy();
+});
+
+test(`Green-led class when unlocked`, () => {
+  const { getByText } = render(<Display locked={false} />);
+  expect(getByText(/Unlocked/i).classList.contains(`green-led`)).toBe(true);
+});
+
+//this is how to check props
+test(`Show closed if closed porp is true, open if false`, () => {
+  const { getByText, queryByText } = render(<Display closed={true} />);
+  expect(getByText(/Closed/i));
+  expect(queryByText(/Open/i)).toBeFalsy();
+});
