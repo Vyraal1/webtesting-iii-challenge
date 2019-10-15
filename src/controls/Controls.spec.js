e372e149b63847ba911019a29f0244d3af29590e
Vyraal1 <kevin.afable@gmail.com>
2019-10-15 17:31:32 -0400

    getByText over and over again

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test(`Lock gate if locked`, () => {
+  const { getByText } = render(<Controls locked={false} />);
+  expect(getByText(/Lock Gate/i));
+});
+
+//do I also need to do one for locked={true}?
+
+test(`toggle button is disabled on lock`, () => {
+  const { getByText } = render(<Controls locked={true} closed={true} />);
+  expect(getByText(/Open Gate/i).disabled).toBe(true);
+});
+
+test(`button says close gate if closed state is false`, () => {
+  const { getByText } = render(<Controls closed={false} />);
+  expect(getByText(/Close Gate/i));
+});
